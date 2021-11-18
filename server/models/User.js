9ff56bd0e6eeec3b72fb106a094125e1f706f8c2
robertgeica <geicarobert@gmail.com>
2021-11-18 22:49:52 +0200

    add User mongoose schema

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    username: { type: String, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    emailVerificationToken: { type: String },
+    emailVerificationExpire: { type: Date },
+    isVerified: { type: Boolean },
+
+    password: { type: String, required: true },
+    passwordResetToken: { type: String },
+    passwordResetExpire: { type: Date },
+
+    collections: [
+      {
+        collectionId: { type: Object, required: true },
+        collectionName: { type: String, required: true },
+      },
+    ],
+
+    profileId: { type: Object },
+    schemaVersion: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = User = mongoose.model("user", UserSchema);
