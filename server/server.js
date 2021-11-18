418b58173ad07da674020e88a69f56a2702324b1
robertgeica <geicarobert@gmail.com>
2021-11-19 00:52:54 +0200

    add auth route in server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ extended: false }));
 
 // routes
 app.use('/api/register', require('./routes/api/register'));
-
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
