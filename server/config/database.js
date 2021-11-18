d475faf5e4db052d1ccc13a4a3cea721554cbc7a
robertgeica <geicarobert@gmail.com>
2021-11-18 22:20:44 +0200

    connect to mongo with mongoose function

--- server/config/database.js
+++ server/config/database.js
@@ -2,3 +2,16 @@ const mongoose = require("mongoose");
 
 const db = process.env.MONGODB_URI || "mongodb://127.0.0.1/doocli";
 
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database connected ...`)
+  } catch (error) {
+    console.log(`Error connecting. ${error.message}`);
+  }
+};
+
+module.exports = connectDb;
