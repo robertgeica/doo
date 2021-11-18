8a60ba1a2f7b5e9ff4b9ccb9f98022f60b3bbf0b
robertgeica <geicarobert@gmail.com>
2021-11-18 23:36:52 +0200

    fix typo error

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new Schema(
     ],
 
     profileId: { type: Object },
-    schemaVersion: { type: String, required },
+    schemaVersion: { type: String, required: true },
   },
   { timestamps: true }
 );
