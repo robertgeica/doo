476241a25ce025ff877441965bfb961ed2ccb2b7
robertgeica <geicarobert@gmail.com>
2021-11-20 15:28:47 +0200

    add password encryption

--- server/models/User.js
+++ server/models/User.js
@@ -24,5 +24,12 @@ const UserSchema = new Schema(
   { timestamps: true }
 );
 
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = User = mongoose.model("User", UserSchema);
