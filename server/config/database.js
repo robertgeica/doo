629483eb1008d1825b828edd5a2c8f723de13b73
robertgeica <geicarobert@gmail.com>
2021-11-20 14:15:48 +0200

    add database connection url in log

--- server/config/database.js
+++ server/config/database.js
@@ -4,11 +4,12 @@ const db = process.env.MONGODB_URI || "mongodb://127.0.0.1/doocli";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(db, {
+    const conn = await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`Database connected ...`)
+
+    console.log(`Database connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error connecting. ${error.message}`);
   }
