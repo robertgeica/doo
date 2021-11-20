ac3017305afdf4f000be4853a1650cebce3c1d1d
robertgeica <geicarobert@gmail.com>
2021-11-20 16:00:45 +0200

    add matchPass method in user schema

--- server/models/User.js
+++ server/models/User.js
@@ -32,4 +32,9 @@ UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.matchPasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = User = mongoose.model("User", UserSchema);
