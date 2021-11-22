39152dbd5cc27a98b9c01b2da265e05c68c89d3a
robertgeica <geicarobert@gmail.com>
2021-11-22 19:47:36 +0200

    update auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,20 +1,33 @@
+const asyncHandler = require("../utils/asyncHandler");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const User = require("../models/User");
 
-module.exports = (req, res, next) => {
-  const token = req.header("auth-token");
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-  if (!token)
-    return res.status(401).json({ msg: "No token. Authentication denied!" });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded.user;
-    next();
-    
-  } catch (error) {
-    console.log(error);
-    res.status(401).json({ msg: "Invalid token." });
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
   }
-};
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = protect;
