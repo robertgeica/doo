36eb4d093221a6873cafadb62e82a55e6a88e900
robertgeica <geicarobert@gmail.com>
2021-11-22 23:08:19 +0200

    update routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require('../middleware/auth');
-const { registerUser, authUser, updateUser } = require('../controllers/userController');
+const { registerUser, authUser, updateUser, deleteUser } = require('../controllers/userController');
 const { createProfile } = require('../controllers/profileController');
 
-router.route('/register').post(registerUser)
-router.route('/login').post(authUser)
-router.route('/:id').put(protect, updateUser)
-router.route('/profile').post(protect, createProfile)
+router
+  .route('/register')
+  .post(registerUser)
+
+router
+  .route('/login')
+  .post(authUser)
+
+router
+  .route('/:id')
+  .put(protect, updateUser)
+  .delete(protect, deleteUser)
+
+router
+  .route('/profile')
+  .post(protect, createProfile)
 
 module.exports = router;
