cee92ce0467b20a282e79fbd1a75c78baaebd729
robertgeica <geicarobert@gmail.com>
2021-11-26 00:54:45 +0200

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
 import HomeScreen from './screens/HomeScreen';
+import Login from './screens/Login';
+import { loadUser } from './actions/userActions';
+
+if (localStorage.token) setAuthToken(localStorage.token);
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
+
+
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<HomeScreen />} />
+        <Route exact path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
