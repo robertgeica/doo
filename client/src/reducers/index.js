8274d2c5dd0d501d482687a9bfd1ba2f9ee57476
robertgeica <geicarobert@gmail.com>
2021-11-26 00:53:43 +0200

    add user reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
-
+import { userReducer } from "./userReducers";
 export default combineReducers({
+  userReducer: userReducer,
   
 });
---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,39 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_LOAD_REQUEST,
+  USER_LOAD_SUCCESS,
+  USER_LOAD_FAIL
+} from "../constants/userConstants";
+
+const userState = {
+  loading: false,
+  user: null,
+  error: null,
+};
+
+export const userReducer = (state = userState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_LOGIN_REQUEST:
+    case USER_REGISTER_REQUEST:
+    case USER_LOAD_REQUEST:
+      return { loading: true };
+    case USER_LOGIN_SUCCESS:
+    case USER_REGISTER_SUCCESS:
+    case USER_LOAD_SUCCESS:
+      return { loading: false, user: payload };
+    case USER_LOGIN_FAIL:
+    case USER_REGISTER_FAIL:
+    case USER_LOAD_FAIL:
+      return { loading: false, error: payload };
+    default:
+      return state;
+  }
+};
