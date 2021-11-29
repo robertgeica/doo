e054894b6f1ee64a0cb7a1f286aee7f26ba7eca2
robertgeica <geicarobert@gmail.com>
2021-11-30 01:03:37 +0200

    add get, create, update and delete settings controllers

---
+++ server/controllers/settingsController.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const Settings = require("../models/Settings");
+const asyncHandler = require("../utils/asyncHandler");
+const ErrorHandler = require('../utils/errorHandler');
+
+// @route         GET /api/user/settings/:id
+// @description   Get user settings
+// @access        Private
+const getSettings = asyncHandler(async (req, res, next) => {
+  const settings = await Settings.findById(req.params.id);
+
+  if (settings) {
+    res.json({
+      ...settings._doc
+    });
+  } else {
+    return next(new ErrorHandler("Invalid", 401));
+  }
+});
+
+// @route         POST /api/user/settings/:userId
+// @description   Create user settings
+// @access        Private
+const createSettings = asyncHandler(async (req, res, next) => {
+  const settingsExists = await Settings.find({ userId: mongoose.Types.ObjectId(req.params.userId) });
+  console.log(settingsExists.length)
+
+  if (settingsExists.length !== 0) {
+    return next(new ErrorHandler('Settings already exists.', 400));
+  }
+
+  const settings = new Settings({
+    ...req.body,
+    userId: req.user._id,
+    schemaVersion: "1.0.0",
+  });
+
+  const createdSettings = await settings.save();
+  res.status(201).json(createdSettings);
+});
+
+// @route         PUT /api/settings/profile/:id
+// @description   Update user settings
+// @access        Private
+const updateSettings = asyncHandler(async (req, res, next) => {
+  const { preferences, theme } = req.body;
+  
+  const settings = await Settings.findById(req.params.id);
+
+  if (settings) {
+    settings.preferences = {...settings.preferences, ...preferences };
+    settings.theme = {...settings.theme, ...theme };
+
+    const updatedSettings = await settings.save();
+    res.json(updatedSettings);
+  } else {
+    return next(new ErrorHandler('Settings not found.', 404));
+  }
+});
+
+// @route         Delete /api/settings/:id
+// @description   Delete settings
+// @access        Private
+const deleteSettings = asyncHandler(async (req, res, next) => {
+  const settings = await Settings.findById(req.params.id);
+
+  if (settings) {
+    await settings.remove()
+    res.json({ message: 'Settings removed' })
+  } else {
+    return next(new ErrorHandler('Settings not found.', 404));
+  }
+});
+
+
+module.exports = { getSettings, createSettings, updateSettings, deleteSettings };
