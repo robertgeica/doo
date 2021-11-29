b114a69c050e99e50fd577050581fb884102aa92
robertgeica <geicarobert@gmail.com>
2021-11-30 00:32:45 +0200

    update user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const protect = require('../middleware/auth');
 const { registerUser, authUser, updateUser, deleteUser, getUser, verifyEmail, sendVerificationEmail, sendResetPasswordEmail, resetPassword } = require('../controllers/userController');
-const { createProfile, updateProfile, deleteProfile } = require('../controllers/profileController');
+const { createProfile, updateProfile, deleteProfile, getProfile } = require('../controllers/profileController');
 
 router
   .route('/register')
@@ -14,37 +14,38 @@ router
   .post(authUser)
 
 router
-  .route('/')
+.route('/resetpassword')
+.patch(sendResetPasswordEmail)
+
+router
+  .route('/:id')
   .get(protect, getUser)
 
 router
   .route('/:id')
-  .put(protect, updateUser)
+  .patch(protect, updateUser)
   .delete(protect, deleteUser)
 
 router
-  .route('/verifyemail')
+  .route('/sendverificationemail/:id')
   .patch(protect, sendVerificationEmail)
 
 router
-  .route('/verifyemail/:id')
+  .route('/verifyemail/:id/:token')
   .patch(protect, verifyEmail)
 
 router
-  .route('/resetpassword')
-  .patch(protect, sendResetPasswordEmail)
+  .route('/resetpassword/:id/:token')
+  .patch(resetPassword)
 
 router
-  .route('/resetpassword/:id')
-  .patch(protect, resetPassword)
-
-router
-  .route('/profile')
+  .route('/profile/:userId')
   .post(protect, createProfile)
 
 router
   .route('/profile/:id')
-  .put(protect, updateProfile)
+  .get(protect, getProfile)
+  .patch(protect, updateProfile)
   .delete(protect, deleteProfile)
 
 module.exports = router;
