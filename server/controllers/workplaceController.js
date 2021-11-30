8ccb1f298eaa602730b14bbef4e28395bbdbab26
robertgeica <geicarobert@gmail.com>
2021-12-01 00:00:38 +0200

    call updateUserWithWorkplace() in createWorkplace controller

--- server/controllers/workplaceController.js
+++ server/controllers/workplaceController.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
 const Workplace = require("../models/Workplace");
 const asyncHandler = require("../utils/asyncHandler");
-const ErrorHandler = require('../utils/errorHandler');
+const ErrorHandler = require("../utils/errorHandler");
+const { updateUserWithWorkplace } = require("./userController");
 
 // @route         GET /api/user/workplace/:id
 // @description   Get workplace
@@ -11,7 +11,7 @@ const getWorkplace = asyncHandler(async (req, res, next) => {
 
   if (workplace) {
     res.json({
-      ...workplace._doc
+      ...workplace._doc,
     });
   } else {
     return next(new ErrorHandler("Invalid", 401));
@@ -22,18 +22,28 @@ const getWorkplace = asyncHandler(async (req, res, next) => {
 // @description   Create workplace
 // @access        Private
 const createWorkplace = asyncHandler(async (req, res, next) => {
-  const workplaceExists = await Workplace.find({ workplaceName: req.body.workplaceName });
+  const workplaceExists = await Workplace.find({
+    workplaceName: req.body.workplaceName,
+  });
   if (workplaceExists.length !== 0) {
-    return next(new ErrorHandler('Workplace already exists.', 400));
+    return next(new ErrorHandler("Workplace already exists.", 400));
   }
 
   const workplace = new Workplace({
     ...req.body,
-    userId: req.userId,
+    userId: req.body.userId,
     schemaVersion: "1.0.0",
   });
 
   const createdWorkplace = await workplace.save();
+  updateUserWithWorkplace({
+    params: {
+      userId: req.body.userId,
+      workplaceId: createdWorkplace._id,
+      workplaceName: createdWorkplace.workplaceName,
+    },
+  });
+
   res.status(201).json(createdWorkplace);
 });
 
@@ -46,13 +56,17 @@ const updateWorkplace = asyncHandler(async (req, res, next) => {
 
   if (workplace) {
     workplace.workplaceName = workplaceName || workplace.workplaceName;
-    workplace.collections = collections ? [ ...workplace.collections, ...collections ] : workplace.collections;
-    workplace.favorites = favorites ? [ ...workplace.favorites, ...favorites ] : workplace.favorites;
+    workplace.collections = collections
+      ? [...workplace.collections, ...collections]
+      : workplace.collections;
+    workplace.favorites = favorites
+      ? [...workplace.favorites, ...favorites]
+      : workplace.favorites;
 
     const updatedWorkplace = await workplace.save();
     res.json(updatedWorkplace);
   } else {
-    return next(new ErrorHandler('Profile not found.', 404));
+    return next(new ErrorHandler("Profile not found.", 404));
   }
 });
 
@@ -63,11 +77,16 @@ const deleteWorkplace = asyncHandler(async (req, res, next) => {
   const workplace = await Workplace.findById(req.params.id);
 
   if (workplace) {
-    await workplace.remove()
-    res.json({ message: 'Workplace removed' })
+    await workplace.remove();
+    res.json({ message: "Workplace removed" });
   } else {
-    return next(new ErrorHandler('Workplace not found.', 404));
+    return next(new ErrorHandler("Workplace not found.", 404));
   }
 });
 
-module.exports = { getWorkplace, createWorkplace, updateWorkplace, deleteWorkplace };
+module.exports = {
+  getWorkplace,
+  createWorkplace,
+  updateWorkplace,
+  deleteWorkplace,
+};
