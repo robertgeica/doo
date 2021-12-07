c72f874b0a3e25d96d1ba3994c009d7e36b388c2
robertgeica <geicarobert@gmail.com>
2021-12-08 01:54:28 +0200

    update user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,75 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require('../middleware/auth');
-const { registerUser, authUser, updateUser, deleteUser, getUser, verifyEmail, sendVerificationEmail, sendResetPasswordEmail, resetPassword } = require('../controllers/userController');
-const { getProfile, createProfile, updateProfile, deleteProfile } = require('../controllers/profileController');
-const { getSettings, createSettings, updateSettings, deleteSettings } = require('../controllers/settingsController');
-const { getWorkplace, createWorkplace, updateWorkplace, deleteWorkplace } = require('../controllers/workplaceController');
+const protect = require("../middleware/auth");
+const {
+  registerUser,
+  authUser,
+  updateUser,
+  deleteUser,
+  getUser,
+  verifyEmail,
+  sendVerificationEmail,
+  sendResetPasswordEmail,
+  resetPassword,
+} = require("../controllers/userController");
+const {
+  getProfile,
+  createProfile,
+  updateProfile,
+  deleteProfile,
+} = require("../controllers/profileController");
+const {
+  getSettings,
+  createSettings,
+  updateSettings,
+  deleteSettings,
+} = require("../controllers/settingsController");
+
+
+router.route("/register").post(registerUser);
+router.route("/login").post(authUser);
+
+router.route("/resetpassword").patch(sendResetPasswordEmail);
+
+router.route("/:id").get(protect, getUser);
+router.route("/:id").patch(protect, updateUser).delete(protect, deleteUser);
 
 router
-  .route('/register')
-  .post(registerUser)
+  .route("/sendverificationemail/:id")
+  .patch(protect, sendVerificationEmail);
 
-router
-  .route('/login')
-  .post(authUser)
+router.route("/verifyemail/:id/:token").patch(protect, verifyEmail);
+router.route("/resetpassword/:id/:token").patch(resetPassword);
 
+router.route("/profile/:userId").post(protect, createProfile);
 router
-.route('/resetpassword')
-.patch(sendResetPasswordEmail)
-
-router
-  .route('/:id')
-  .get(protect, getUser)
-
-router
-  .route('/:id')
-  .patch(protect, updateUser)
-  .delete(protect, deleteUser)
-
-router
-  .route('/sendverificationemail/:id')
-  .patch(protect, sendVerificationEmail)
-
-router
-  .route('/verifyemail/:id/:token')
-  .patch(protect, verifyEmail)
-
-router
-  .route('/resetpassword/:id/:token')
-  .patch(resetPassword)
-
-router
-  .route('/profile/:userId')
-  .post(protect, createProfile)
-
-router
-  .route('/profile/:id')
+  .route("/profile/:id")
   .get(protect, getProfile)
   .patch(protect, updateProfile)
-  .delete(protect, deleteProfile)
+  .delete(protect, deleteProfile);
 
+router.route("/settings/:userId").post(protect, createSettings);
 router
-  .route('/settings/:userId')
-  .post(protect, createSettings)
-
-router
-  .route('/settings/:id')
+  .route("/settings/:id")
   .get(protect, getSettings)
   .patch(protect, updateSettings)
-  .delete(protect, deleteSettings)
-
-router
-  .route('/workplace/:userId')
-  .post(protect, createWorkplace)
-
-router
-  .route('/workplace/:id')
-  .get(protect, getWorkplace)
-  .patch(protect, updateWorkplace)
-  .delete(protect, deleteWorkplace)
-
-
+  .delete(protect, deleteSettings);
 
 module.exports = router;
