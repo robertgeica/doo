2cd264b134837b8beb8e44a0298a57584043e81a
robertgeica <geicarobert@gmail.com>
2021-12-07 23:36:49 +0200

    add collection schema

---
+++ server/models/Collection.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CollectionSchema = new Schema(
+  {
+    schemaVersion: { type: String, required: true, default: "1.0.0" },
+    userId: { type: Object },
+    workplaceId: { type: Object },
+
+    name: { type: String },
+    icon: { type: String },
+    background: { type: String },
+
+    comments: [
+      {
+        accoundId: { type: Object },
+        accountName: { type: String },
+        content: { type: String },
+      },
+    ],
+
+    labels: [
+      {
+        text: { type: String },
+        color: { type: String },
+      },
+    ],
+
+    blocks: [
+      {
+        blockId: { type: Object },
+        blockName: { type: String },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = Collection = mongoose.model("Collection", CollectionSchema);
