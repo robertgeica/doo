668addc24e6ce04d55699621cf49dddf2b11c877
robertgeica <geicarobert@gmail.com>
2022-02-08 22:17:14 +0200

    [cli] workplace api req

---
+++ cli/api/workplace.js
@@ -0,0 +1,110 @@
+const axios = require("axios");
+const keytar = require("keytar");
+const { getUser } = require('./auth');
+const { apiSuccess, apiError } = require('../ui/api');
+const { addWorkplaceSuccess } = require('../ui/workplace');
+const { getProfile } = require('./profile');
+
+const addWorkplace = async (name) => {
+  const user = await getUser();
+  const token = await keytar.getPassword("doocli", "token");
+
+
+  if(user) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+
+    const newWorkplace = {
+      workplaceName: name,
+      collections: [],
+      favorites: []
+    }
+
+    const body = JSON.stringify(newWorkplace);
+    try {
+      const res = await axios.post(
+        `http://localhost:4000/api/workplace/${user._id}`,
+        body,
+        config
+      );
+  
+      addWorkplaceSuccess(`New workplace added: ${res.data.createdWorkplace.workplaceName}!`)
+    } catch (error) {
+      if (error) apiError('Sorry, something went wrong.');
+    }
+  };
+  
+}
+
+const getWorkplaces = async () => {
+  const user = await getUser();
+  const workplacesNames = user.workplacesIds.map(wp => wp.name)
+  return workplacesNames;
+}
+
+const getWorkplace = async () => {
+  const profile = await getProfile();
+  const token = await keytar.getPassword("doocli", "token");
+  const workplaceId = profile.defaults.workplace;
+  
+  if(profile && workplaceId) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+
+    try {
+      const res = await axios.get(
+        `http://localhost:4000/api/workplace/${workplaceId}`,
+        config
+      );
+
+      return res.data.workplaceName;
+    } catch (error) {
+      if (error) apiError('Sorry, something went wrong.');
+    }
+  }
+}
+
+const setWorkplace = async (name) => {
+  const user = await getUser();
+  const token = await keytar.getPassword("doocli", "token");
+  const workplace = user.workplacesIds.filter(wp => wp.name === name);
+
+  if(user && workplace) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    };
+
+    const id = workplace[0].workplaceId;
+    const profileId = user.profileId;
+    try {
+      const res = await axios.get(
+        `http://localhost:4000/api/workplace/${id}`,
+        config
+      );
+
+      // console.log(res.data);
+      await axios.patch(
+        `http://localhost:4000/api/user/profile/${profileId}`,
+        { defaults: { workplace: res.data._id } },
+        config,
+      );
+        
+      console.log("workplace added w success");
+    } catch (error) {
+      if (error) apiError('Sorry, something went wrong.');
+    }
+  }
+}
+
+module.exports = { getWorkplaces, getWorkplace, addWorkplace, setWorkplace };
