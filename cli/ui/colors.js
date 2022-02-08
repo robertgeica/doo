251bea06711dc7814a93f6c23fa3aa11e6082f5b
robertgeica <geicarobert@gmail.com>
2022-02-08 22:12:44 +0200

    add ui colors file

---
+++ cli/ui/colors.js
@@ -0,0 +1,7 @@
+const error = 'e74c3c';
+const success = '2ecc71';
+const normal = 'fff';
+const pending = "f1c40f";
+const workplace = '2980b9';
+
+module.exports = { error, success, normal, pending, workplace };
