3375c156cf098c12a23b4a6b76b283c691965081
robertgeica <geicarobert@gmail.com>
2022-02-08 21:54:16 +0200

    [cli] update app.js

--- cli/app.js
+++ cli/app.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
-const { registerUser, loginUser, getUser, getWorkplaces } = require("./actions");
+const { registerUser, loginUser, getUser } = require('./apiRequests/auth');
+const { addWorkplace, getWorkplaces, setWorkplace, getWorkplace } = require('./apiRequests/workplace');
+const { userAccountInfo } = require('./ui/user');
+const { renderWorkplaces, renderWorkplace } = require('./ui/workplace');
 
 // register user
 yargs.command({
@@ -23,22 +26,64 @@ yargs.command({
   },
 });
 
+// get user
 yargs.command({
   command: 'user',
   describe: 'user',
 
   async handler() {
-    getUser();
+    const user = await getUser();
+    userAccountInfo(user);
   }
 });
 
+//add workplace
 yargs.command({
-  command: 'workplace',
-  describe: 'workplace',
+  command: 'aw',
+  describe: 'add workplace',
 
-  async handler() {
-    getWorkplaces();
+  async handler(argv) {
+    addWorkplace(argv._[1]);
   }
 })
 
+//get workplaces
+yargs.command({
+  command: 'vws',
+  describe: 'view workplaces',
+
+  async handler() {
+    const workplaces = await getWorkplaces();
+    renderWorkplaces(workplaces);
+  }
+})
+
+//set workplaces
+yargs.command({
+  command: 'sw',
+  describe: 'set workplace',
+
+  async handler(argv) {
+    setWorkplace(argv._[1]);
+  }
+})
+
+// view workplace
+yargs.command({
+  command: 'vw',
+  describe: 'view current workplace',
+
+  async handler() {
+    const crtWorkplace = await getWorkplace();
+    renderWorkplace(crtWorkplace);
+  }
+})
+
+// edit workplace
+
+// delete workplace
+
+
+
+/* logout user */
 yargs.parse();
