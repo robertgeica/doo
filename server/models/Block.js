1324697ac73fc68b4292e35324ee4c90f6f4f5a8
robertgeica <geicarobert@gmail.com>
2022-02-17 19:07:42 +0200

    [be] add block schema

---
+++ server/models/Block.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BlockSchema = new Schema(
+  {
+    schemaVersion: { type: String, required: true, default: "1.0.0" },
+    userId: { type: Object },
+
+    icon: { type: String },
+    background: { type: String },
+
+    blockName: { type: String },
+    textFormat: {
+      textColor: { type: String },
+      backgroundColor: { type: String },
+    },
+
+    comments: [
+      {
+        accoundId: { type: Object },
+        accountName: { type: String },
+        content: { type: String },
+      },
+    ],
+
+    blockType: { type: String },
+    blockContent: { type: Object },
+  },
+  { timestamps: true }
+);
+
+module.exports = Block = mongoose.model("Block", BlockSchema);
