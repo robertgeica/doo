9bd3635bd5b6aa6fd09279762fdafc7db273a8fa
robertgeica <geicarobert@gmail.com>
2022-02-17 19:44:18 +0200

    add Dropdown component

---
+++ client/src/layout/components/Dropdown/Dropdown.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+const Dropdown = (props) => {
+  const { options, value, onChange, placeholder, itemActions } = props;
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggling = () => setIsOpen(!isOpen);
+
+  const [defaultValue, setDefaultValue] = useState(value ? value.label : placeholder);
+
+  console.log(props);
+  return (
+    <div className="main">
+      <div className="dropdown-container">
+        {itemActions ? itemActions?.addItem() : null}
+        <div className="dropdown-header" onClick={toggling}>
+          {defaultValue}
+        </div>
+        {isOpen && (
+          <div className="dropdown-list-container">
+            <ul className="dropdown-list" onClick={toggling}>
+              {options.map((option) => (
+                <div className="dropdown-list-item-container">
+                  <li
+                    className="dropdown-list-item"
+                    onClick={(e) => {
+                      onChange(e);
+                      setDefaultValue(option.label);
+                    }}
+                    id={option.value}
+                  >
+                    {option.label}
+                  </li>
+                  {itemActions ? (
+                    <span className="item-actions">
+                      {itemActions?.deleteItem(option.value)}
+                      {itemActions?.updateItem()}
+                    </span>
+                  ) : null}
+                </div>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
