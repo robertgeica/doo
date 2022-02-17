9e57eaff5bf405ba335e19f700a5556718007b4d
robertgeica <geicarobert@gmail.com>
2022-02-17 19:35:56 +0200

    [fe] add workplace reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducers";
+import { workplaceReducer } from "./workplaceReducers";
+
 export default combineReducers({
   userReducer: userReducer,
-  
+  workplaceReducer: workplaceReducer
 });
---
+++ client/src/reducers/workplaceReducers.js
@@ -0,0 +1,53 @@
+import {
+  WORKPLACE_LOAD_REQUEST,
+  WORKPLACE_LOAD_SUCCESS,
+  WORKPLACE_LOAD_FAIL,
+  WORKPLACES_LOAD_REQUEST,
+  WORKPLACES_LOAD_SUCCESS,
+  WORKPLACES_LOAD_FAIL,
+  WORKPLACE_ADD_REQUEST,
+  WORKPLACE_ADD_SUCCESS,
+  WORKPLACE_ADD_FAIL,
+  WORKPLACE_UPDATE_REQUEST,
+  WORKPLACE_UPDATE_SUCCESS,
+  WORKPLACE_UPDATE_FAIL,
+  WORKPLACE_DELETE_REQUEST,
+  WORKPLACE_DELETE_SUCCESS,
+  WORKPLACE_DELETE_FAIL,
+} from "../constants/workplaceConstants";
+
+const workplaceState = {
+  loading: false,
+  error: null,
+  workplace: null,
+  workplaces: null,
+};
+
+export const workplaceReducer = (state = workplaceState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case WORKPLACE_LOAD_REQUEST:
+    case WORKPLACES_LOAD_REQUEST:
+    case WORKPLACE_ADD_REQUEST:
+    case WORKPLACE_DELETE_REQUEST:
+    case WORKPLACE_UPDATE_REQUEST:
+      return { ...state, loading: true };
+
+    case WORKPLACE_LOAD_SUCCESS:
+      return { ...state, loading: false, workplace: payload };
+
+    case WORKPLACES_LOAD_SUCCESS:
+      return { ...state, loading: false, workplaces: payload };
+
+    case WORKPLACE_LOAD_FAIL:
+    case WORKPLACES_LOAD_FAIL:
+    case WORKPLACE_ADD_FAIL:
+    case WORKPLACE_DELETE_FAIL:
+    case WORKPLACE_UPDATE_FAIL:
+      return { loading: false, error: payload };
+
+    default:
+      return state;
+  }
+};
