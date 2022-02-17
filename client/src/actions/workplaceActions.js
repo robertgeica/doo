744967f6399a8cd1a74058b43814e8d3c5267dc0
robertgeica <geicarobert@gmail.com>
2022-02-17 19:37:42 +0200

    [fe] add workplace actions

---
+++ client/src/actions/workplaceActions.js
@@ -0,0 +1,120 @@
+import axios from "axios";
+import {
+  WORKPLACE_LOAD_REQUEST,
+  WORKPLACE_LOAD_SUCCESS,
+  WORKPLACE_LOAD_FAIL,
+  WORKPLACES_LOAD_REQUEST,
+  WORKPLACES_LOAD_SUCCESS,
+  WORKPLACES_LOAD_FAIL,
+  WORKPLACE_ADD_REQUEST,
+  WORKPLACE_ADD_SUCCESS,
+  WORKPLACE_ADD_FAIL,
+  WORKPLACE_UPDATE_REQUEST,
+  WORKPLACE_UPDATE_SUCCESS,
+  WORKPLACE_UPDATE_FAIL,
+  WORKPLACE_DELETE_REQUEST,
+  WORKPLACE_DELETE_SUCCESS,
+  WORKPLACE_DELETE_FAIL,
+} from "../constants/workplaceConstants";
+
+export const loadWorkplace = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: WORKPLACE_LOAD_REQUEST });
+
+    const { data } = await axios.get(
+      `http://localhost:4000/api/workplace/${id}`
+    );
+
+    dispatch({
+      type: WORKPLACE_LOAD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: WORKPLACE_LOAD_FAIL });
+  }
+};
+
+export const loadWorkplaces = (userWorkplaces) => async (dispatch) => {
+  console.log(userWorkplaces);
+
+  try {
+    dispatch({ type: WORKPLACES_LOAD_REQUEST });
+    const data = userWorkplaces.map((workplace) => {
+      return { workplaceId: workplace.workplaceId, name: workplace.name };
+    });
+
+    dispatch({
+      type: WORKPLACES_LOAD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: WORKPLACES_LOAD_FAIL });
+  }
+};
+
+export const addWorkplace = (workplace, userId) => async (dispatch) => {
+  try {
+    dispatch({ type: WORKPLACE_ADD_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const data = await axios.post(
+      `http://localhost:4000/api/workplace/${userId}`,
+      { ...workplace },
+      config
+    );
+
+    dispatch({
+      type: WORKPLACE_ADD_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: WORKPLACE_ADD_FAIL });
+  }
+};
+
+export const deleteWorkplace = (workplaceId) => async (dispatch) => {
+  try {
+    dispatch({ type: WORKPLACE_DELETE_REQUEST });
+
+    const res = await axios.delete(
+      `http://localhost:4000/api/workplace/${workplaceId}`
+    );
+
+    dispatch({
+      type: WORKPLACE_DELETE_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({ type: WORKPLACE_DELETE_FAIL });
+  }
+};
+
+export const updateWorkplace = (workplace, workplaceId) => async (dispatch) => {
+  try {
+    dispatch({ type: WORKPLACE_UPDATE_REQUEST });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const data = await axios.patch(
+      `http://localhost:4000/api/workplace/${workplaceId}`,
+      { ...workplace },
+      config
+    );
+
+    dispatch({
+      type: WORKPLACE_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: WORKPLACE_UPDATE_FAIL });
+  }
+};
