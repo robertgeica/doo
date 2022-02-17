49f155bb3568e78da71ea2d0e538d7c3cf1adc15
robertgeica <geicarobert@gmail.com>
2022-02-17 19:16:59 +0200

    [cli] add workplace ui

--- cli/ui/workplace.js
+++ cli/ui/workplace.js
@@ -1,22 +1,48 @@
 const chalk = require("chalk");
-const { error, success, normal, workplace } = require('./colors');
+const { error, success, normal, workplace } = require("./colors");
 
-const addWorkplaceSuccess = (input) => {
+const renderWorkplaces = (workplacesArr) => {
+  console.log(chalk.hex(normal)(`These are your workplaces:`));
+
+  workplacesArr.forEach((wp) => {
+    wp.isDefault
+      ? console.log(chalk.hex(workplace).bold(`@${wp.name}`))
+      : console.log(chalk.hex(workplace)(`@${wp.name}`));
+  });
   console.log(
-    chalk.hex(success)(
-      `${input}`)
-  )
+    chalk.hex(normal)(
+      `Use 'doo sw [workplaceName]' to set a current workplace.`
+    )
+  );
 };
 
-const renderWorkplaces = workplacesArr => {
-  console.log(chalk.hex(normal)(`These are your workplaces:`));
-  workplacesArr.forEach(wp => {
-    console.log(chalk.hex(workplace).bold(`@${wp}`));
-  });
-  console.log(chalk.hex(normal)(`Use 'doo sw [workplaceName]' to set a current workplace.`));
-}
+const renderWorkplace = (crtWorkplace) => {
+  const icon = "💾";
+  console.log(
+    `${icon} ${chalk.hex(workplace).bold(`@${crtWorkplace.workplaceName}`)}`
+  );
+  renderFavorites(crtWorkplace.favorites);
+  renderCollections(crtWorkplace.collections);
+};
 
-const renderWorkplace = crtWorkplace => {
-  console.log(chalk.hex(workplace).bold(`@${crtWorkplace}`));
-}
-module.exports = { addWorkplaceSuccess, renderWorkplaces, renderWorkplace }
+const renderCollections = (collectionsArr) => {
+  const line = "----------";
+  const icon = "🔵";
+
+  console.log(line);
+  collectionsArr.map((col) =>
+    console.log(
+      `  ${icon} ${chalk.hex(workplace).bold(`@${col.collectionName}`)}`
+    )
+  );
+};
+
+const renderFavorites = (favoritesArr) => {
+  const icon = "💙";
+
+  favoritesArr.map((fav) =>
+    console.log(`  ${icon} ${chalk.hex(workplace).bold(`@${fav.blockName}`)}`)
+  );
+};
+
+module.exports = { renderWorkplaces, renderWorkplace };
